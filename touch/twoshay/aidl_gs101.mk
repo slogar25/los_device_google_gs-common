384d4bde36e29fdaeb560d689c400c89642371ee
Alvin Francis <nivlafx@gmail.com>
2024-06-29 00:57:40 -0400

    gs-common: Skip twoshay device manifest if generated adevtool version exist
    Change-Id: Ib434f6a39f77e9d08b426c807bc0f5fe21c3c280
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- touch/twoshay/aidl_gs101.mk
+++ touch/twoshay/aidl_gs101.mk
@@ -1,2 +1,5 @@
+ifneq ($(wildcard vendor/google_devices/*/vintf/adevtool_manifest_vendor.xml),)
+else
 DEVICE_MANIFEST_FILE += device/google/gs-common/touch/twoshay/aidl/manifest_gs101.xml
+endif
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/touch/twoshay/aidl/compatibility_matrix_gs101.xml
