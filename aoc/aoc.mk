36ba42dc880768e0fee0110ab45e5f8fd8adf5ef
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-08-22 12:34:59 +0300

    add missing FCM definitions for some proprietary HALs

--- aoc/aoc.mk
+++ aoc/aoc.mk
@@ -7,3 +7,15 @@ PRODUCT_PACKAGES += dump_aoc \
 # If AoC Daemon is not present on this build, load firmware at boot via rc
 PRODUCT_COPY_FILES += \
 	device/google/gs-common/aoc/conf/init.aoc.daemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.aoc.rc
+	
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+	device/google/gs-common/proprietary/aocx.IAocx.xml
+
+# AoC debug support
+PRODUCT_PACKAGES_DEBUG += \
+	aocdump \
+	aocutil \
+	aoc_audio_cfg \
+	vp_util \
+	aocx_tool
+
--- device.mk
+++ device.mk
@@ -32,10 +32,10 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml
 
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += \
-	device/google/gs-common/vintf/framework_compatibility_matrix.xml
-
-DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/proprietary/com.google.flood.xml
+	device/google/gs-common/vintf/framework_compatibility_matrix.xml \
+	device/google/gs-common/proprietary/com.google.flood.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 	device/google/gs-common/proprietary/com.google.android.imageprocessing.hal.xml
+
 
